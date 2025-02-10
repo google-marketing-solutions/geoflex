c1ad59064cc01d662725d91df58f7a43b3e7b29f
Sam Bailey <samcb@google.com>
2025-02-10 08:14:15 -0800

    GeoFleX: Add important objects to top level import.
    Change-Id: I480b622b477fc7d321c98c49b744a8b4eeef486d

--- geoflex/__init__.py
+++ geoflex/__init__.py
@@ -1,3 +1,14 @@
 """GeoFleX: A flexible and unified geo-experiment solution."""
 
+import geoflex.constraints
+import geoflex.data
+import geoflex.experiment
+
+ExperimentDesignConstraints = geoflex.constraints.ExperimentDesignConstraints
+Methodology = geoflex.constraints.Methodology
+ExperimentType = geoflex.constraints.ExperimentType
+GeoPerformanceDataset = geoflex.data.GeoPerformanceDataset
+Experiment = geoflex.experiment.Experiment
+ExperimentDesign = geoflex.experiment.ExperimentDesign
+
 __version__ = "0.0.1"
