edd3a81b01c4df208585fcb534c12a9b9363b1a2
Sergei Dorogin <segy@google.com>
2025-02-26 16:59:38 +0400

    [webapp] webapp scaffolding  (empty generated Quasar/Vue app with Vite/TS)
    Change-Id: I021132ff1d11b6ed4bb344765de9b8dc03d2015c

---
+++ webapp/README.md
@@ -0,0 +1,3 @@
+# GeoFlex WebApp
+
+TODO
---
+++ webapp/src/layouts/MainLayout.vue
@@ -0,0 +1,49 @@
+<template>
+  <q-layout view="hHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          <router-link to="/" style="text-decoration: none; color: white"
+            >GeoFlex Testing Platform</router-link
+          ></q-toolbar-title
+        >
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <SideMenu />
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <div class="text-body1 text-center q-ma-sm">
+      &copy;&nbsp;Google gTech Ads, 2025.
+    </div>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import SideMenu from 'components/SideMenu.vue';
+
+defineOptions({
+  name: 'MainLayout',
+});
+
+const leftDrawerOpen = ref(false);
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+</script>
